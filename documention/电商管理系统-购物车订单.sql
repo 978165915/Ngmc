023276c17bee2ead9811ac89d1a03de7d55eb63f
Yagami <1104820976@qq.com>
2019-09-02 15:12:14 +0800

    购物车数据库

---
+++ documention/电商管理系统-购物车订单.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog v10.2 
+MySQL - 5.7.13 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `shopping trolley` (
+	`goods_id` int (10),
+	`quantity` int (5),
+	`sum` int (10),
+	`join_date` varchar (600)
+); 
