cca39a80918faf1af3b2e57540a555ffa237f0c4
Administrator <978165915@qq.com>
2019-09-02 14:18:37 +0800

    实现新增

--- src/com/neu/demo01/dao/impl/GoodsDaoImpl.java
+++ src/com/neu/demo01/dao/impl/GoodsDaoImpl.java
@@ -11,21 +11,22 @@ import java.util.List;
 public class GoodsDaoImpl extends DBUtil implements GoodsDao {
     @Override
     public int save(Goods goods) throws SQLException {
-        String sql="insert into goods(`name`,`typeid`,`imgpath`,`price`,`goodsDesc`,`createTime`)"+
-                "values(?,?,?,?,?,?)";
-        return executeUpdate(sql,goods.getName(),goods.getTypeid(),goods.getId(),goods.getPrice(),goods.getGoodsDesc(),goods.getCreateTime());
+        String sql="insert into goods(`id`,`name`,`typeid`,`imgpath`,`price`,`goodsDesc`,`createTime`)"+
+                "values(?,?,?,?,?,?,?)";
+        return executeUpdate(sql,goods.getId(),goods.getName(),goods.getTypeid(),goods.getId(),goods.getPrice(),goods.getGoodsDesc(),goods.getCreateTime());
     }
 
     @Override
     public List<Goods> getGoodsList() throws SQLException {
         List<Goods>goodsList=new ArrayList<>();
-        String sql="select `name`,`typeid`,`imgpath`,`price`,`goodsDesc`,`createTime`"
+        String sql="select `id`,`name`,`typeid`,`imgpath`,`price`,`goodsDesc`,`createTime`"
                 +"from Goods";
         try {
             rs = executeQuery(sql);
             Goods goods = null;
             while (rs.next()) {
                 goods = new Goods();
+                goods.setId(rs.getInt("id"));
                 goods.setName(rs.getString("name"));
                 goods.setTypeid(rs.getInt("typeid"));
                 goods.setImgpath(rs.getString("imgpath"));
@@ -43,13 +44,14 @@ public class GoodsDaoImpl extends DBUtil implements GoodsDao {
     @Override
     public List<Goods> getGoodsListByPage(int currentPage, int pageSize)  throws SQLException{
         List<Goods>goodsList=new ArrayList<>();
-        String sql="select `name`,`typeid`,`imgpath`,`price`,`goodsDesc`,`createTime`"
-                +"from Goods limit ?,?";
+        String sql="select `id`,`name`,`typeid`,`imgpath`,`price`,`goodsDesc`,`createTime`"
+                +"from Goods order by createTime desc limit ?,?";
         try {
             rs = executeQuery(sql,(currentPage-1)*pageSize,pageSize);
             Goods goods = null;
             while (rs.next()) {
                 goods = new Goods();
+                goods.setId(rs.getInt("id"));
                 goods.setName(rs.getString("name"));
                 goods.setTypeid(rs.getInt("typeid"));
                 goods.setImgpath(rs.getString("imgpath"));
