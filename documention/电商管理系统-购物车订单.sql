dca92337ad7aed399880b8afc9d7e34dfc396360
bocai <yinbocai@163.com>
2019-09-02 17:42:58 +0800

    购物车表

--- documention/电商管理系统-购物车订单.sql
+++ documention/电商管理系统-购物车订单.sql
@@ -5,9 +5,9 @@ MySQL - 5.7.13
 */
 /*!40101 SET NAMES utf8 */;
 
-create table `shopping trolley` (
-	`goods_id` int (10),
-	`quantity` int (5),
-	`sum` int (10),
-	`join_date` varchar (600)
+create table `shopping_car` (
+	`goods_id` int (11),
+	`num` int (5),
+	`userid` int (11),
+	`create_date` varchar (19)/*2019-09-01 12:00:01*/
 ); 
