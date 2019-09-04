b9c2f53034d78997769a1957673db45eac33e619
bocai <yinbocai@163.com>
2019-09-04 16:37:45 +0800

    订单列表详细信息

---
+++ src/com/neu/demo01/dao/impl/OrderDetailDaoImpl.java
@@ -0,0 +1,34 @@
+package com.neu.demo01.dao.impl;
+
+import com.neu.demo01.dao.OrderDetailDao;
+import com.neu.demo01.entity.OrderItem;
+import com.neu.demo01.util.DBUtil;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDetailDaoImpl extends DBUtil implements OrderDetailDao {
+    @Override
+    public List<OrderItem> getItemsByOrderId(int id) throws SQLException {
+        List<OrderItem> orderItems = new ArrayList<>();
+        String sql = "SELECT ogd.*,g.`name`,g.`imgpath`" +
+                " FROM `order_goods_detail` ogd,`goods` g" +
+                " WHERE ogd.`itemid`=g.`id` AND `orderid`=?";
+        rs = executeQuery(sql,id);
+        OrderItem orderItem = null;
+        while(rs.next()){
+            orderItem = new OrderItem();
+            orderItem.setId(rs.getInt("id"));
+            orderItem.setItemid(rs.getInt("itemid"));
+            orderItem.setOrderid(id);
+            orderItem.setNum(rs.getInt("num"));
+            orderItem.setPrice(rs.getDouble("price"));
+            orderItem.setTotal(rs.getDouble("total"));
+            orderItem.setGoodsname(rs.getString("name"));
+            orderItem.setGoodsimg(rs.getString("imgpath"));
+            orderItems.add(orderItem);
+        }
+        return orderItems;
+    }
+}
